4da1fe40500dd57d5b906a2c08d3deca1be6e267
Rachel McGuigan <saturnsmoon64@gmail.com>
2025-09-03 16:57:45 -0700

    Update main.tf

--- infra/dev/main.tf
+++ infra/dev/main.tf
@@ -183,24 +183,29 @@ output "cluster_certificate_authority_data" {
 ##############################
 # Kubernetes & Helm Providers
 ##############################
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_name
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_name
+}
 
 data "aws_eks_cluster_auth" "eks" {
   name = module.eks.cluster_name
 }
 
 provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  token                  = data.aws_eks_cluster_auth.eks.token
-
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
 }
 
 provider "helm" {
   kubernetes {
-    host                   = module.eks.cluster_endpoint
-    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-    token                  = data.aws_eks_cluster_auth.eks.token
-    
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
   }
 }
 
@@ -210,7 +215,6 @@ provider "helm" {
 
 resource "kubernetes_namespace" "argocd" {
   metadata { name = "argocd" }
-  depends_on = [module.eks]
 }
 
 resource "helm_release" "argocd" {
