43cbd0f0289e872d9261adce34432dd128605ef1
Rachel McGuigan <saturnsmoon64@gmail.com>
2025-09-06 19:20:55 -0700

    plastic survery:

--- infra/dev/main.tf
+++ infra/dev/main.tf
@@ -5,6 +5,14 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.20"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.9"
+    }
     random = {
       source  = "hashicorp/random"
       version = "~> 3.0"
@@ -12,15 +20,15 @@ terraform {
     local = {
       source  = "hashicorp/local"
       version = "~> 2.0"
-
     }
   }
 }
 
+##################################
+### Random / Local Example Resources
+##################################
 
-resource "random_pet" "name" {
-  length = 2
-}
+resource "random_pet" "name" { length = 2 }
 
 resource "local_file" "demo" {
   filename = "demo-output.txt"
@@ -30,21 +38,20 @@ resource "local_file" "demo" {
 output "demo_message" {
   value = local_file.demo.content
 }
-#### EKS CLUSTER ###
 
+##################################
+### EKS Cluster IAM Roles
+##################################
 
-# EKS cluster IAM role
+# Cluster Role
 resource "aws_iam_role" "eks_cluster_role" {
   name = "eks-cluster-role-ai-dev"
-
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
-      Action = "sts:AssumeRole"
-      Principal = {
-        Service = "eks.amazonaws.com"
-      }
-      Effect = "Allow"
+      Action    = "sts:AssumeRole"
+      Principal = { Service = "eks.amazonaws.com" }
+      Effect    = "Allow"
     }]
   })
 }
@@ -54,18 +61,15 @@ resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
-# Node group IAM role
+# Node Group Role
 resource "aws_iam_role" "eks_node_role" {
   name = "eks-node-role-ai-dev"
-
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
-      Action = "sts:AssumeRole"
-      Principal = {
-        Service = "ec2.amazonaws.com"
-      }
-      Effect = "Allow"
+      Action    = "sts:AssumeRole"
+      Principal = { Service = "ec2.amazonaws.com" }
+      Effect    = "Allow"
     }]
   })
 }
@@ -80,53 +84,68 @@ resource "aws_iam_role_policy_attachment" "node_AmazonEKS_CNI_Policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
 }
 
-resource "aws_iam_role_policy_attachment" "node_AmazonEC2ContainerRegistryReadOnly" {
+resource "aws_iam_role_policy_attachment" "node_ECR_ReadOnly" {
   role       = aws_iam_role.eks_node_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
 
-# VPC + Subnets
+##################################
+### VPC
+##################################
+
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.1.2"
 
-  name = "eks-ai-dev-vpc"
-  cidr = "10.0.0.0/16"
-
-  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
+  name                 = "eks-ai-dev-vpc"
+  cidr                 = "10.0.0.0/16"
+  azs                  = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets       = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
   enable_nat_gateway   = true
   single_nat_gateway   = true
   enable_dns_hostnames = true
   enable_dns_support   = true
 
-  tags = {
-    Environment = "dev"
-    Give = "Get"
-  }
+  tags = { Environment = "dev" }
 }
 
-# EKS Cluster
+##################################
+### EKS Cluster
+##################################
+
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 20.0"
 
   cluster_name    = "ai-demo"
   cluster_version = "1.32"
-  
-  subnet_ids = module.vpc.private_subnets
-  vpc_id     = module.vpc.vpc_id
-
-  tags = {
-    Environment = "dev"
-  }
-  cluster_endpoint_public_access  = true
+  subnet_ids      = module.vpc.private_subnets
+  vpc_id          = module.vpc.vpc_id
+  cluster_endpoint_public_access      = true
   enable_cluster_creator_admin_permissions = true
 
+  tags = { Environment = "dev" }
+
+  eks_managed_node_groups = {
+    dev_nodes = {
+      desired_size   = 2
+      min_size       = 1
+      max_size       = 3
+      instance_types = ["t3.large"]
+      capacity_type  = "SPOT"
+      iam_role_arn   = aws_iam_role.eks_node_role.arn
+      tags = { Environment = "dev" }
+    }
+  }
+
+  cluster_addons = {
+    coredns   = { most_recent = true }
+    kube-proxy = { most_recent = true }
+    vpc-cni    = { most_recent = true }
+  }
+
   access_entries = {
-    # One access entry with a policy associated
     example = {
       kubernetes_groups = []
       principal_arn     = aws_iam_role.eks_node_role.arn
@@ -135,124 +154,223 @@ module "eks" {
         example = {
           policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSViewPolicy"
           access_scope = {
-            namespaces = ["default", "aidemo"]
+            namespaces = ["default", "aidemo", "argocd"]
             type       = "namespace"
           }
         }
       }
     }
   }
+}
 
-  cluster_addons = {
-    coredns = {
-      most_recent = true
-    }
-    kube-proxy = {
-      most_recent = true
-    }
-    vpc-cni = {
-      most_recent = true
-    }
-  }
-    # EKS Managed Node Group(s)
-  eks_managed_node_group_defaults = {
-    instance_types = ["m6i.large", "m5.large", "m5n.large", "m5zn.large"]
-  }
+output "cluster_name" { value = module.eks.cluster_name }
+output "cluster_endpoint" { value = module.eks.cluster_endpoint }
+output "cluster_certificate_authority_data" { value = module.eks.cluster_certificate_authority_data }
 
-  eks_managed_node_groups = {
-    dev_nodes = {
-      min_size     = 1
-      max_size     = 3
-      desired_size = 2
+##################################
+### Kubernetes & Helm Providers
+##################################
 
-      instance_types = ["t3.large"]
+data "aws_eks_cluster" "main" { name = module.eks.cluster_name }
+data "aws_eks_cluster_auth" "main" { name = module.eks.cluster_name }
 
-      capacity_type = "SPOT"
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.main.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.main.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.main.token
+}
 
-      iam_role_arn = aws_iam_role.eks_node_role.arn
-
-      tags = {
-        Environment = "dev"
-      }
-    }
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.main.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.main.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.main.token
   }
 }
 
-output "cluster_name" {
-  value = module.eks.cluster_name
+##################################
+### Kubernetes Namespaces
+##################################
+
+resource "kubernetes_namespace" "aws_lb_controller" {
+  metadata { name = "aws-load-balancer-controller" }
+  depends_on = [module.eks]
 }
 
-output "cluster_endpoint" {
-  value = module.eks.cluster_endpoint
+resource "kubernetes_namespace" "argocd" {
+  metadata { name = "argocd" }
+  depends_on = [module.eks]
 }
 
-output "cluster_certificate_authority_data" {
-  value = module.eks.cluster_certificate_authority_data
+##################################
+### Helm Releases
+##################################
+
+resource "helm_release" "aws_lb_controller" {
+  name       = "aws-load-balancer-controller"
+  namespace  = kubernetes_namespace.aws_lb_controller.metadata[0].name
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+  version    = "1.10.3"
+  depends_on = [module.eks, kubernetes_namespace.aws_lb_controller]
 }
 
-### route53 ###
-# Fetch your existing VPC
-data "aws_vpc" "eks_vpc" {
-  id = module.vpc.vpc_id  # Replace with your cluster VPC
+data "aws_acm_certificate" "argocd" {
+  domain      = "argocd.designcodemonkey.space"
+  statuses    = ["ISSUED"]
+  most_recent = true
 }
 
-# Fetch the hosted zone (private)
-data "aws_route53_zone" "private_zone" {
-  name         = "designcodemonkey.com" 
-  #private_zone = true
+resource "aws_acm_certificate_validation" "argocd" {
+  certificate_arn         = data.aws_acm_certificate.argocd.arn
+  validation_record_fqdns = ["argocd.designcodemonkey.space"]
 }
 
-resource "aws_route53_vpc_association_authorization" "auth" {
-  vpc_id        = data.aws_vpc.eks_vpc.id
-  zone_id       = data.aws_route53_zone.private_zone.id
+data "aws_secretsmanager_secret_version" "github_oauth" {
+  secret_id = "github_oauth"
 }
 
-resource "aws_route53_zone_association" "assoc" {
-  zone_id = data.aws_route53_zone.private_zone.id
-  vpc_id  = data.aws_vpc.eks_vpc.id
-}
-# Associate the private zone
+locals { github_oauth = jsondecode(data.aws_secretsmanager_secret_version.github_oauth.secret_string) }
 
-resource "aws_route53_zone_association" "eks_private_zone" {
-  zone_id = data.aws_route53_zone.private_zone.id
-  vpc_id  = data.aws_vpc.eks_vpc.id
-}
-
-## we need to deploy our  application called portfolio
-
-#Ecr build repository
-resource "aws_ecr_repository" "portfolio" {
-  name = "portfolio"
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-}
-
-
-#route53 entry
-#helm chart refernece
-
-data "aws_eks_cluster" "main" {
-  name = "ai-demo"
-}
-
-data "aws_eks_cluster_auth" "main" {
-  name = data.aws_eks_cluster.main.name
-}
-
-resource "helm_release" "portfolio" {
-  name       = "portfolio"
-  namespace  = "portfolio"
-  repository = "https://raw.githubusercontent.com/turtlelovesshoes/waffle-scaling-ai-infra/main/k8s/portfolio-helm"
-  chart      = "portfolio"
-  version    = "0.1.0"
+resource "helm_release" "argocd" {
+  name             = "argocd"
+  namespace        = kubernetes_namespace.argocd.metadata[0].name
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-cd"
+  version          = "8.3.1"
+  create_namespace = false
+  wait             = true
+  cleanup_on_fail  = true
+  depends_on       = [module.eks, kubernetes_namespace.argocd]
 
   values = [
     yamlencode({
-      image = {
-        repository = "391767403730.dkr.ecr.us-west-2.amazonaws.com/portfolio"
-        tag        = "rachelm-deploysite-48544db4eefbf6df03bd831743a041c318cb59fd"
+      server = {
+        ingress = {
+          enabled          = true
+          ingressClassName = "alb"
+          hosts            = ["argocd.designcodemonkey.space"]
+          paths            = ["/"]
+          pathType         = "Prefix"
+          https = { enabled = true, servicePort = 443 }
+          annotations = {
+            "alb.ingress.kubernetes.io/scheme"           = "internet-facing"
+            "alb.ingress.kubernetes.io/listen-ports"    = "[{\"HTTPS\":443}]"
+            "alb.ingress.kubernetes.io/certificate-arn" = data.aws_acm_certificate.argocd.arn
+            "alb.ingress.kubernetes.io/ssl-redirect"    = "443"
+            "alb.ingress.kubernetes.io/target-type"     = "ip"
+            "alb.ingress.kubernetes.io/group.name"      = "argocd-alb-target-group"
+            "kubernetes.io/ingress.class"               = "alb"
+          }
+        }
+      }
+      dex = {
+        connectors = [{
+          type   = "github"
+          id     = "github"
+          name   = "GitHub"
+          config = {
+            clientID     = local.github_oauth.github_oauth_client_id
+            clientSecret = local.github_oauth.github_oauth_client_secret
+            orgs         = [{ name = "turtlelovesshoes" }]
+          }
+        }]
+      }
+      rbac = { policyCSV = "g, turtlelovesshoes, role:admin" }
+      repoServer = {
+        defaultRepos = [{
+          url    = "https://github.com/turtlelovesshoes/waffle-scaling-ai-infra.git"
+          path   = "k8s/"
+          branch = "main"
+        }]
       }
     })
   ]
+}
+## missing ##
+# 1. ACM Certificate for your domain
+resource "aws_acm_certificate" "argocd_cert" {
+  domain_name       = "argocd.designcodemonkey.space"
+  validation_method = "DNS"
+
+  tags = {
+    Name = "argocd-cert"
+  }
+}
+
+# 2. Route 53 Zone (existing)
+data "aws_route53_zone" "main" {
+  name         = "designcodemonkey.space."
+  private_zone = false
+}
+
+# 3. Route 53 DNS Record for Certificate Validation
+resource "aws_route53_record" "argocd_cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.argocd_cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      type   = dvo.resource_record_type
+      record = dvo.resource_record_value
+    }
+  }
+
+  zone_id = data.aws_route53_zone.main.zone_id
+  name    = each.value.name
+  type    = each.value.type
+  ttl     = 300
+  records = [each.value.record]
+}
+
+# 4. Validate ACM Certificate
+resource "aws_acm_certificate_validation" "argocd_cert_validation" {
+  certificate_arn         = aws_acm_certificate.argocd_cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.argocd_cert_validation : record.fqdn]
+}
+
+# 5. IAM Role for ArgoCD Controller
+resource "aws_iam_role" "argocd_controller_role" {
+  name = "argocd-controller-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"  # EKS service account if using IRSA
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+# 6. IAM Policy for Controller
+resource "aws_iam_policy" "argocd_controller_policy" {
+  name        = "argocd-controller-policy"
+  description = "Policy for ArgoCD to manage ACM & Route53 DNS validation"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = [
+          "acm:DescribeCertificate",
+          "acm:ListCertificates",
+          "acm:GetCertificate",
+          "route53:ChangeResourceRecordSets",
+          "route53:ListResourceRecordSets",
+          "route53:GetHostedZone"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+# 7. Attach IAM Policy to Role
+resource "aws_iam_role_policy_attachment" "argocd_attach" {
+  role       = aws_iam_role.argocd_controller_role.name
+  policy_arn = aws_iam_policy.argocd_controller_policy.arn
 }
